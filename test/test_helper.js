37b9108deff77b73b9b5442b0e85f07cfb16bce6
Julian Nicholls <juliannicholls29@gmail.com>
2016-08-17 19:46:58 +0100

    Update test_helper as per lectures in section 3

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,36 +1,51 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
-import jsdom from 'jsdom';
-import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from '../src/reducers';
+import React              from 'react';
+import ReactDOM           from 'react-dom';
+import { createStore }    from 'redux';
+import { Provider }       from 'react-redux';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
+import TestUtils          from 'react-addons-test-utils';
 
-chaiJquery(chai, chai.util, $);
+import jsdom              from 'jsdom';
+import jquery             from 'jquery';
+import chai, { expect }   from 'chai';
+import chaiJquery         from 'chai-jquery';
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
+import reducers           from '../src/reducers';
+
+// Set up testing environment to run like a browser from the command line
+
+global.document = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
+global.window   = global.document.defaultView;
+
+const $         = jquery(global.window);  // Connect our jquery to our 'window'
+
+// Render a React component for testing
+
+export function renderComponent(ComponentClass, props = {}, state = {}) {
+  const instance = TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
   );
 
-  return $(ReactDOM.findDOMNode(componentInstance));
+  // Return a jquery wrapped set of HTML.
+  return $(ReactDOM.findDOMNode(instance));
 }
 
+// Simulate an event on a jquery element
+
 $.fn.simulate = function(eventName, value) {
-  if (value) {
+  if(value) {
     this.val(value);
   }
-  TestUtils.Simulate[eventName](this[0]);
-};
 
-export {renderComponent, expect};
+  TestUtils.Simulate[eventName](this[0]);
+}
+
+// Set up Chai jQuery
+
+chaiJquery(chai, chai.util, $);
+
+// Export(s)
+
+export { expect };
